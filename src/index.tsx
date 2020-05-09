080546afd09b3388b440d26bbc74667c75091b0d
Liisi Kinnunen <liisi.kinnunen@aalto.fi>
2020-05-09 20:51:50 +0300

    Add react bootstrap

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
+/* Importing the Bootstrap CSS */
+/* eslint-disable-next-line import/no-unassigned-import*/
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import { App } from "./components/app";
 import * as serviceWorker from "./service-worker";
