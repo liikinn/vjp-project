b11f885ba9fe9436073f03db355b52b3f18fcc5f
Liisi Kinnunen <liisi.kinnunen@aalto.fi>
2020-05-10 21:50:41 +0300

    Adjust zoom level in map.

--- src/components/map-component.tsx
+++ src/components/map-component.tsx
@@ -7,7 +7,7 @@ import "./map-component.css";
 const LeafletMap: React.FC = () => {
   const position: [number, number] = [60.20064, 24.903831];
   return (
-    <Map className="map" center={position} zoom={12}>
+    <Map className="map" center={position} zoom={13}>
       <TileLayer
         url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
         attribution="&copy; <a href='http://osm.org/copyright'>OpenStreetMap</a> contributors"
