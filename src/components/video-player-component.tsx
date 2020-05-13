cf055426faa7fe53cd86bd8628945903ca7d1faa
Liisi Kinnunen <liisi.kinnunen@aalto.fi>
2020-05-13 18:39:18 +0300

    Add thumbnail to video player.

--- src/components/video-player-component.tsx
+++ src/components/video-player-component.tsx
@@ -2,7 +2,23 @@ import React from "react";
 import ReactPlayer from "react-player";
 
 const VideoPlayer: React.FC = () => {
-  return <ReactPlayer url="H4-video.mp4" width="100%" height="100%" controls />;
+  return (
+    <div
+      style={{
+        position: "relative",
+        paddingTop: "56.25%" /* Player ratio: 100 / (1280 / 720) */,
+      }}
+    >
+      <ReactPlayer
+        style={{ position: "absolute", top: 0, left: 0 }}
+        light="thumbnail.png"
+        url="H4-video.mp4"
+        width="100%"
+        height="100%"
+        controls
+      />
+    </div>
+  );
 };
 
 export { VideoPlayer };
